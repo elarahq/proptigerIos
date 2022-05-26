da510205881793868fd1665fbba8f8bd7d05c22c
kshitij <kshitij.gulati@housing.com>
2022-05-26 23:48:17 +0530

    Initial Commit

---
+++ Proptiger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Proptiger
+//
+//  Created by Kshitij Gulati on 26/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proptiger/ProptigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProptigerApp.swift
+//  Proptiger
+//
+//  Created by Kshitij Gulati on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ProptigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
